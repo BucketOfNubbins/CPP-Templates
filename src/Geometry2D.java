da29a184949ad5b22f0d33f7f96c493550c3c027
Collin Jakubik <38540308+BucketOfNubbins@users.noreply.github.com>
2022-12-09 14:06:19 -0600

    updates

--- src/Geometry2D.java
+++ src/Geometry2D.java
@@ -21,4 +21,19 @@ public class Geometry2D {
         return Math.abs(area);
     }
 
+    // creates new vector from a to b
+    private static double[] difference(double[] a, double[] b) {
+        return new double[]{b[0] - a[0], b[1] - a[1]};
+    }
+
+    private static boolean segmentIntersection(double[] a, double[] b, double[] p, double[] q) {
+        double[] ab = difference(a, b);
+        double[] ap = difference(a, p);
+        double[] aq = difference(a, q);
+        double[] pq = difference(p, q);
+        double[] pa = difference(p, a);
+        double[] pb = difference(p, b);
+        return crossProduct(ab, ap) * crossProduct(ab, aq) < 0 && crossProduct(pq, pa) * crossProduct(pq, pb) < 0;
+    }
+
 }

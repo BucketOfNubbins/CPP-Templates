6bc05f1aafb32a2f934b8fc256a365de0990aee7
Collin Jakubik <38540308+BucketOfNubbins@users.noreply.github.com>
2022-12-09 18:51:30 -0600

    updates

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        String output = "";
-        for (int i = 0; i < 25; i++) {
-            output += chars.charAt((int) (Math.random() * chars.length()));
-        }
-        System.out.println(output);
+
 
         br.close();
         bw.close();
